864002ef8fc17faf672550644e65eff41ea23be4
Lan Haide <lanhaide@hotmail.com>
2019-07-11 16:06:08 +0900

    todo list complete with fake data source and delegate methods

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by 蘭海徳 on 2019/07/10.
+//  Copyright © 2019 promisecode. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy GameObject"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+}
+
